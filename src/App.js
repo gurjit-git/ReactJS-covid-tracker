46419a3b6f76cb91d42834a08df811ec16b01069
WordPress Experts <40169845+gurjit-git@users.noreply.github.com>
2022-01-18 17:02:54 +0530

    New Files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Cards, Charts, CountryPicker } from './components';
+
 function App() {
   return (
     <div className="App">
         Covid-19 Tracker
+        <Cards />
+        <Charts />
+        <CountryPicker />
     </div>
   );
 }
