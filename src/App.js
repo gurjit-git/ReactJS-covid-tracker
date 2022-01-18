372d0e3ba5d5c7366052db45cfd1c4157a675cc6
WordPress Experts <40169845+gurjit-git@users.noreply.github.com>
2022-01-18 18:29:50 +0530

    Import Files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css'; 
 import { Cards, Charts, CountryPicker } from './components';
 
+//import { fetchData } from './api';
+const url = 'https://covid19.mathdro.id/api';
+
 function App() {
+
+  const [apiData, setApiData] = useState(null);
+  
+  useEffect( () => {
+
+    const fetchData = async () => {
+      try {
+          const response = await axios(url);
+          //return response.data;
+          setApiData( response.data )
+      }
+      catch(error){
+  
+      }
+    }
+    
+    fetchData();
+
+  }, [] );
+
+  console.log(apiData);
+
   return (
     <div className="App">
         Covid-19 Tracker
