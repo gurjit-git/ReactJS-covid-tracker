cf76a6699af6a01c3cd2db2a9f5add17c204b27f
WordPress Experts <40169845+gurjit-git@users.noreply.github.com>
2022-01-19 20:40:45 +0530

    Update the Cards Data

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,20 +1,18 @@
 import { Container, Grid, CardContent, Typography, Card } from "@material-ui/core";
 import CountUp from 'react-countup';
-import classNames from 'classnames';
+import cx from 'classnames';
 
 import styles from './Cards.module.css';
 
-let cx = classNames.bind(styles);
-
 const Cards = ( props ) => {
-    console.log(props.data);
+
     if(!props.data){
         return "Loading.....";
     }
     
     return (
         <Container maxWidth="md">
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid container spacing={3} justify="center">
                 <Grid item  xs={12} md={4} component={Card} className={cx(styles.card, styles.confirmed)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
@@ -24,7 +22,7 @@ const Cards = ( props ) => {
                            <CountUp start={0} end={ props.data.confirmed.value } duration={2.5} separator="," />
                         </Typography>  
                         <Typography color="textSecondary" gutterBottom>
-                            
+                            { new Date(props.data.lastUpdate).toDateString() }
                         </Typography>  
                         <Typography  variant="body2">
                             Number of active cased of Covid-19
@@ -40,7 +38,7 @@ const Cards = ( props ) => {
                            <CountUp start={0} end={ props.data.recovered.value } duration={2.5} separator="," />
                         </Typography>  
                         <Typography color="textSecondary" gutterBottom>
-                           
+                            { new Date(props.data.lastUpdate).toDateString() }
                         </Typography>  
                         <Typography  variant="body2">
                             Number of recoveries from Covid-19
@@ -56,7 +54,7 @@ const Cards = ( props ) => {
                         <CountUp start={0} end={ props.data.deaths.value } duration={2.5} separator="," />
                         </Typography>  
                         <Typography color="textSecondary" gutterBottom>
-                           
+                            { new Date(props.data.lastUpdate).toDateString() }
                         </Typography>  
                         <Typography  variant="body2">
                             Number of deaths caused by Covid-19
